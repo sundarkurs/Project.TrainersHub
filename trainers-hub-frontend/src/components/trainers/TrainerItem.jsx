33bee125a1b6a43fed786a0b402c84bb67aa6a14
Sundar Urs <sundarkurs@gmail.com>
2019-01-25 11:28:36 +0530

    Update TrainerItem.jsx

--- trainers-hub-frontend/src/components/trainers/TrainerItem.jsx
+++ trainers-hub-frontend/src/components/trainers/TrainerItem.jsx
@@ -9,18 +9,20 @@ class TrainerItem extends Component {
 
   render() {
 
-    var cardCustomization = {
-        
+    var customAvatar = {
+      height: '100'
     };
 
     return (
-        <div className="card" style={cardCustomization}>
-            <img className="card-img-top" src="images/default-trainer-avatar.jpg" alt="Card image"/>
+        <div className="card">
+            <img className="card-img-top" style={customAvatar} src="images/default-trainer-avatar.jpg" alt="Trainer picture"/>
             <div className="card-body">
-                <h4 className="card-title">{this.props.trainer.firstName}</h4>
-                <p className="card-text">{this.props.trainer.lastname}</p>
-                <a href="#" onClick={this.deleteTrainer.bind(this, this.props.trainer.id)} 
-                className="btn btn-danger">Delete</a>
+              <h4 className="card-title">{this.props.trainer.firstName}</h4>
+              <p className="card-text">{this.props.trainer.lastname}</p>
+            </div>
+            <div className="card-footer" >
+              <a href="#" className="btn btn-info">View</a>
+              <a href="#" onClick={this.deleteTrainer.bind(this, this.props.trainer.id)} className="btn btn-danger">Delete</a>
             </div>
         </div>
     );
